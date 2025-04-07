2a317a5be8c467cb4d812cbdc0090bdded8bc13e
JMarceloAL <joaomarcl.251@gmail.com>
2025-04-07 18:42:47 -0300

    aplicativo filalizado pronto  para build

--- src/routes/CustomDrawer.js
+++ src/routes/CustomDrawer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Linking } from 'react-native'
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
 import Foundation from '@expo/vector-icons/Foundation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -14,6 +14,14 @@ const CustomDrawer = (props) => {
 
             <DrawerContentScrollView  {...props}>
                 <DrawerItemList {...props}></DrawerItemList>
+                <TouchableOpacity style={{ marginLeft: 15, marginTop: 10 }} onPress={() => {
+
+                    Linking.openURL('https://docs.google.com/forms/d/e/1FAIpQLScJEVPxvW58NJOnU_TBAXzNpNj2PkCBedMjVkq71AKKStGoCQ/viewform?usp=dialog')
+                }}>
+
+                    <Text style={{ fontSize: 20, color: "#6b73c7" }}>Avalie</Text>
+
+                </TouchableOpacity>
             </DrawerContentScrollView>
             <View style={{ flex: 1, justifyContent: "flex-end" }}>
 
